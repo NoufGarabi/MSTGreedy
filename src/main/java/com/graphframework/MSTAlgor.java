597f8cfb2ab4827a6572bf55a17b5f5c10874182
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-04-16 01:17:59 +0300

    added getters and setters to MSTAlgor class

--- src/main/java/com/graphframework/MSTAlgor.java
+++ src/main/java/com/graphframework/MSTAlgor.java
@@ -3,5 +3,17 @@ package com.graphframework;
 public abstract class MSTAlgor {
     private Edge[] MSTResultList;
 
+    public Edge[] getMSTResultList() {
+        return MSTResultList;
+    }
+
+    public void setMSTResultList(Edge[] mSTResultList) {
+        MSTResultList = mSTResultList;
+    }
+
+    public MSTAlgor(Edge[] mSTResultList) {
+        MSTResultList = mSTResultList;
+    }
+
     public abstract void displayResultingMST();
 }

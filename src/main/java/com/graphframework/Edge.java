edf6c6f25ad79796a2f4dcb4cbb97b6896c4d1a0
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-05-18 00:04:37 +0300

    edited display edge info method in edge class

--- src/main/java/com/graphframework/Edge.java
+++ src/main/java/com/graphframework/Edge.java
@@ -51,7 +51,11 @@ public class Edge {
     public void displayInfo() {
         // System.out.println("Edge information:");
         System.out.print("Source vertex: " + getSource().getLabel());
+<<<<<<< HEAD
         System.out.print(" Target vertex: " + getTarget().getLabel());
+=======
+        System.out.print(" Target vertex: " + getTarget());
+>>>>>>> e0fc26d (edited display edge info method in edge class)
         System.out.println(" Edge weight: " + getWeight());
     }
 }

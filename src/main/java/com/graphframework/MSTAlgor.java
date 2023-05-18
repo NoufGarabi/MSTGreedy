0a2a50b349e2b9888fde3d27326d3a7a91dcb9df
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-05-19 00:06:33 +0300

    added comments to MSTALGOR class

--- src/main/java/com/graphframework/MSTAlgor.java
+++ src/main/java/com/graphframework/MSTAlgor.java
@@ -1,21 +1,19 @@
 /**
-
-The MSTAlgor class is an abstract class representing an algorithm for finding
-the minimum spanning tree (MST) of a graph.
-It provides a framework for implementing specific MST algorithms.
-*/
+ * The MSTAlgor class is an abstract class representing an algorithm for finding
+ * the minimum spanning tree (MST) of a graph.
+ * It provides a framework for implementing specific MST algorithms.
+ */
 package com.graphframework;
+
 import com.phonenetworkapp.Line;
 
 public abstract class MSTAlgor {
-private Line[] MSTResultList; // Stores the resulting MST edges
+    private Line[] MSTResultList; // Stores the resulting MST edges
 
-
-/**
- * Abstract method to display the resulting minimum spanning tree.
- * Each specific MST algorithm must implement this method.
- */
-public abstract void displayResultingMST();
-
-
+    /**
+     * Abstract method to display the resulting minimum spanning tree.
+     * Each specific MST algorithm must implement this method.
+     * It should display the MST in a meaningful way, such as printing the edges or visualizing the graph.
+     */
+    public abstract void displayResultingMST();
 }

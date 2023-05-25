0bbffec8e41602fd16ec8878243a3dc0f2355f49
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-05-25 18:08:49 +0300

    edited the docomentation of line class

--- src/main/java/com/PhoneNetworkApp/Line.java
+++ src/main/java/com/PhoneNetworkApp/Line.java
@@ -1,18 +1,39 @@
+/**
+
+The Line class represents an edge between two vertices in a phone network graph.
+It extends the Edge class from the graph framework.
+*/
 package com.phonenetworkapp;
 
 import com.graphframework.Edge;
 import com.graphframework.Vertex;
 
+/**
+ * 
+ * The Line class represents an edge between two vertices in a phone network
+ * graph.
+ */
 public class Line extends Edge {
-    private int lLength;
-    private Vertex source;
-    private Vertex target;
+    private int lLength; // The length of the line
+    private Vertex source; // The source vertex of the line
+    private Vertex target; // The target vertex of the line
 
+    /**
+     * 
+     * Constructs a new Line object.
+     */
     public Line() {
-
     }
 
-    // Constructor
+    /**
+     * 
+     * Constructs a new Line object with the specified source, destination, and
+     * weight.
+     * 
+     * @param source      The source vertex of the line.
+     * @param destination The target vertex of the line.
+     * @param weight      The weight of the line.
+     */
     public Line(Vertex source, Vertex destination, int weight) {
         super(source, destination, weight);
         this.lLength = weight * 5;
@@ -20,14 +41,31 @@ public class Line extends Edge {
         this.target = destination;
     }
 
-    public int getlLength() {
+    /**
+     * 
+     * Returns the length of the line.
+     * 
+     * @return The length of the line.
+     */
+    public int getLength() {
         return lLength;
     }
 
-    public void setlLength(int lLength) {
-        this.lLength = lLength;
+    /**
+     * 
+     * Sets the length of the line.
+     * 
+     * @param length The length of the line.
+     */
+    public void setLength(int length) {
+        this.lLength = length;
     }
 
+    /**
+     * 
+     * Displays information about the line, including the source, target, and
+     * length.
+     */
     @Override
     public void displayInfo() {
         System.out.print("Office No." + (char) (Integer.parseInt(this.getSource().getLabel()) + 65));
@@ -35,20 +73,43 @@ public class Line extends Edge {
         System.out.println(": line length: " + this.lLength);
     }
 
+    /**
+     * 
+     * Returns the source vertex of the line.
+     * 
+     * @return The source vertex of the line.
+     */
     public Vertex getSource() {
         return source;
     }
 
+    /**
+     * 
+     * Sets the source vertex of the line.
+     * 
+     * @param source The source vertex of the line.
+     */
     public void setSource(Office source) {
         this.source = source;
     }
 
+    /**
+     * 
+     * Returns the target vertex of the line.
+     * 
+     * @return The target vertex of the line.
+     */
     public Vertex getTarget() {
         return target;
     }
 
+    /**
+     * 
+     * Sets the target vertex of the line.
+     * 
+     * @param target The target vertex of the line.
+     */
     public void setTarget(Office target) {
         this.target = target;
     }
-
 }

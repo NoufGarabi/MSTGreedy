538ef5c346a11786ac338a8ba4acfad4be2561e2
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-05-25 18:16:46 +0300

    edited docomentation for route class

--- src/main/java/com/airfreighapp/Route.java
+++ src/main/java/com/airfreighapp/Route.java
@@ -1,17 +1,38 @@
+/**
+
+The Route class represents an edge in an air freight network graph that represents a route between two locations.
+It extends the Edge class from the graph framework.
+*/
 package com.airfreighapp;
 
 import com.graphframework.Edge;
 import com.graphframework.Vertex;
 
-public class Route extends Edge{
+/**
+ * 
+ * The Route class represents an edge in an air freight network graph that
+ * represents a route between two locations.
+ */
+public class Route extends Edge {
 
+    /**
+     * 
+     * Constructs a new Route object with the specified source, target, and weight.
+     * 
+     * @param source The source vertex of the route.
+     * @param target The target vertex of the route.
+     * @param weight The weight of the route.
+     */
     public Route(Vertex source, Vertex target, int weight) {
         super(source, target, weight);
     }
 
+    /**
+     * 
+     * Displays information about the route, including its length.
+     */
     @Override
     public void displayInfo() {
         System.out.println("--- route length: " + this.getWeight());
     }
-    
 }

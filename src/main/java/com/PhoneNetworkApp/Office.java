8179073d6ee63fc347a1a967eb54acfe9e05de59
Nouf Khaled <NoufAlgarabi@gmail.com>
2023-05-25 18:10:06 +0300

    edited the docmentation of office class

--- src/main/java/com/PhoneNetworkApp/Office.java
+++ src/main/java/com/PhoneNetworkApp/Office.java
@@ -1,21 +1,46 @@
+/**
+
+The Office class represents a vertex in a phone network graph that represents an office.
+It extends the Vertex class from the graph framework.
+*/
 package com.phonenetworkapp;
 
 import com.graphframework.Vertex;
 
+/**
+ * 
+ * The Office class represents a vertex in a phone network graph that represents
+ * an office.
+ */
 public class Office extends Vertex {
 
+    /**
+     * 
+     * Constructs a new Office object with the specified label.
+     * 
+     * @param label The label of the office.
+     */
     public Office(String label) {
         super(label);
-
     }
 
-    // Method to set the correct value within the relevant constructor
+    /**
+     * 
+     * Sets the label of the office.
+     * Overrides the setLabel method in the Vertex class to set the label in the
+     * correct format.
+     * 
+     * @param label The label of the office.
+     */
     @Override
     public void setLabel(String label) {
         super.setLabel("O" + label);
     }
 
-    
+    /**
+     * 
+     * Displays information about the office, including its label.
+     */
     @Override
     public void displayInfo() {
         System.out.println("Office No." + (char) (Integer.parseInt(getLabel() + 65)) + " ");
